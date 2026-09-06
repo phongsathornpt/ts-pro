970a6c0aa58c923274828f01fbba4a8947aabcec
thorn <37927965+phongsathornpt@users.noreply.github.com>
2026-09-06 19:57:43 +0700

    refactor: split arm64 lowering

---
+++ internal/backend/lower/arm64.go
@@ -0,0 +1,612 @@
+package lower
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/phongsathornpt/ts-pro/internal/backend/asm/arm64"
+	"github.com/phongsathornpt/ts-pro/internal/backend/regalloc"
+	"github.com/phongsathornpt/ts-pro/internal/core/ir"
+)
+
+func lowerARM64(prog *ir.Program) ([]byte, error) {
+	e := arm64.NewEmitter()
+
+	fnOffsets := make(map[string]int)
+	var callFixups []callFixup
+	var branchFixups []branchFixupARM64
+	var strFixups []stringFixupARM64
+	bbOffsets := make(map[*ir.BasicBlock]int)
+
+	for _, fn := range prog.Functions {
+		fnOffsets[fn.Name] = len(e.Code)
+
+		ra := regalloc.New(len(arm64ScratchRegs))
+		locs := ra.Allocate(fn)
+
+		// Prologue: save FP, LR, and callee-saved registers X19-X24
+		e.SubImm(arm64.SP, arm64.SP, 80)
+		e.Stp(arm64.X29, arm64.X30, arm64.SP, 64)
+		e.AddImm(arm64.X29, arm64.SP, 64)
+		e.Stp(arm64.X19, arm64.X20, arm64.SP, 48)
+		e.Stp(arm64.X21, arm64.X22, arm64.SP, 32)
+		e.Stp(arm64.X23, arm64.X24, arm64.SP, 16)
+
+		// Copy incoming parameters into allocated locations
+		for i, param := range fn.Params {
+			if i < len(arm64ParamRegs) {
+				loc := locs[param.ID]
+				if loc.IsReg {
+					targetReg := arm64ScratchRegs[loc.Reg]
+					e.MovReg(targetReg, arm64ParamRegs[i])
+				}
+			}
+		}
+
+		// Emit basic blocks
+		for _, bb := range fn.Blocks {
+			bbOffsets[bb] = len(e.Code)
+
+			for _, inst := range bb.Instructions {
+				switch bi := inst.(type) {
+				case *ir.BinaryInst:
+					dstLoc := locs[bi.Res.ID]
+					if !dstLoc.IsReg {
+						continue
+					}
+					dstReg := arm64ScratchRegs[dstLoc.Reg]
+
+					// LHS
+					var lhsReg arm64.Register = arm64.X8
+					if vLHS, ok := bi.LHS.(*ir.Value); ok {
+						srcLoc := locs[vLHS.ID]
+						if srcLoc.IsReg {
+							lhsReg = arm64ScratchRegs[srcLoc.Reg]
+						}
+					} else if cLHS, ok := bi.LHS.(ir.ConstNumber); ok {
+						if cLHS.Value < 0 {
+							e.Movz(arm64.X8, uint16(-cLHS.Value))
+							e.Sub(arm64.X8, arm64.XZR, arm64.X8)
+						} else {
+							e.Movz(arm64.X8, uint16(cLHS.Value))
+						}
+						lhsReg = arm64.X8
+					}
+
+					// RHS
+					var rhsReg arm64.Register = arm64.X16
+					if vRHS, ok := bi.RHS.(*ir.Value); ok {
+						rhsLoc := locs[vRHS.ID]
+						if rhsLoc.IsReg {
+							rhsReg = arm64ScratchRegs[rhsLoc.Reg]
+						}
+					} else if cRHS, ok := bi.RHS.(ir.ConstNumber); ok {
+						if cRHS.Value < 0 {
+							e.Movz(arm64.X16, uint16(-cRHS.Value))
+							e.Sub(arm64.X16, arm64.XZR, arm64.X16)
+						} else {
+							e.Movz(arm64.X16, uint16(cRHS.Value))
+						}
+						rhsReg = arm64.X16
+					}
+
+					switch bi.Op {
+					case ir.OpAdd:
+						e.Add(dstReg, lhsReg, rhsReg)
+					case ir.OpSub:
+						e.Sub(dstReg, lhsReg, rhsReg)
+					case ir.OpMul:
+						e.Mul(dstReg, lhsReg, rhsReg)
+					case ir.OpDiv:
+						e.Sdiv(dstReg, lhsReg, rhsReg)
+					case ir.OpMod:
+						e.Sdiv(arm64.X17, lhsReg, rhsReg)
+						e.Mul(arm64.X18, arm64.X17, rhsReg)
+						e.Sub(dstReg, lhsReg, arm64.X18)
+					case ir.OpAnd:
+						e.And(dstReg, lhsReg, rhsReg)
+					case ir.OpOr:
+						e.Orr(dstReg, lhsReg, rhsReg)
+					case ir.OpLt:
+						e.Cmp(lhsReg, rhsReg)
+						e.Cset(dstReg, arm64.CondLT)
+					case ir.OpLe:
+						e.Cmp(lhsReg, rhsReg)
+						e.Cset(dstReg, arm64.CondLE)
+					case ir.OpGt:
+						e.Cmp(lhsReg, rhsReg)
+						e.Cset(dstReg, arm64.CondGT)
+					case ir.OpGe:
+						e.Cmp(lhsReg, rhsReg)
+						e.Cset(dstReg, arm64.CondGE)
+					case ir.OpEq:
+						e.Cmp(lhsReg, rhsReg)
+						e.Cset(dstReg, arm64.CondEQ)
+					case ir.OpNe:
+						e.Cmp(lhsReg, rhsReg)
+						e.Cset(dstReg, arm64.CondNE)
+					}
+
+				case *ir.UnaryInst:
+					if bi.Op != "-" {
+						continue
+					}
+					dstLoc := locs[bi.Res.ID]
+					if !dstLoc.IsReg {
+						continue
+					}
+					dstReg := arm64ScratchRegs[dstLoc.Reg]
+					if v, ok := bi.Val.(*ir.Value); ok {
+						srcLoc := locs[v.ID]
+						if srcLoc.IsReg {
+							e.Sub(dstReg, arm64.XZR, arm64ScratchRegs[srcLoc.Reg])
+						}
+					}
+
+				case *ir.CallInst:
+					for i, arg := range bi.Args {
+						if i < len(arm64ParamRegs) {
+							targetParam := arm64ParamRegs[i]
+							if vArg, ok := arg.(*ir.Value); ok {
+								argLoc := locs[vArg.ID]
+								if argLoc.IsReg {
+									e.MovReg(targetParam, arm64ScratchRegs[argLoc.Reg])
+								}
+							} else if cArg, ok := arg.(ir.ConstNumber); ok {
+								if cArg.Value < 0 {
+									e.Movz(targetParam, uint16(-cArg.Value))
+									e.Sub(targetParam, arm64.XZR, targetParam)
+								} else {
+									e.Movz(targetParam, uint16(cArg.Value))
+								}
+							} else if sArg, ok := arg.(ir.ConstString); ok {
+								strOffset := len(e.Code)
+								e.Adr(targetParam, 0)
+								strFixups = append(strFixups, stringFixupARM64{
+									offset:    strOffset,
+									targetReg: targetParam,
+									str:       sArg.Value,
+								})
+							}
+						}
+					}
+
+					callOffset := len(e.Code)
+					e.Bl(0) // placeholder
+					callFixups = append(callFixups, callFixup{offset: callOffset, callee: bi.Callee})
+
+					if bi.Res != nil {
+						dstLoc := locs[bi.Res.ID]
+						if dstLoc.IsReg {
+							dstReg := arm64ScratchRegs[dstLoc.Reg]
+							e.MovReg(dstReg, arm64.X0)
+						}
+					}
+				}
+			}
+
+			// Terminator
+			if bb.Terminator != nil {
+				switch term := bb.Terminator.(type) {
+				case *ir.ReturnTerm:
+					if term.Val != nil {
+						if v, ok := term.Val.(*ir.Value); ok {
+							loc := locs[v.ID]
+							if loc.IsReg {
+								srcReg := arm64ScratchRegs[loc.Reg]
+								if srcReg != arm64.X0 {
+									e.MovReg(arm64.X0, srcReg)
+								}
+							}
+						} else if c, ok := term.Val.(ir.ConstNumber); ok {
+							if c.Value < 0 {
+								e.Movz(arm64.X0, uint16(-c.Value))
+								e.Sub(arm64.X0, arm64.XZR, arm64.X0)
+							} else {
+								e.Movz(arm64.X0, uint16(c.Value))
+							}
+						} else if s, ok := term.Val.(ir.ConstString); ok {
+							strOffset := len(e.Code)
+							e.Adr(arm64.X0, 0)
+							strFixups = append(strFixups, stringFixupARM64{
+								offset:    strOffset,
+								targetReg: arm64.X0,
+								str:       s.Value,
+							})
+						}
+					}
+
+					if fn.Name == "@main" {
+						callOffset := len(e.Code)
+						e.Bl(0)
+						callFixups = append(callFixups, callFixup{offset: callOffset, callee: "ts_sys_exit"})
+					}
+
+					// Epilogue: restore callee-saved registers and FP/LR
+					e.Ldp(arm64.X23, arm64.X24, arm64.SP, 16)
+					e.Ldp(arm64.X21, arm64.X22, arm64.SP, 32)
+					e.Ldp(arm64.X19, arm64.X20, arm64.SP, 48)
+					e.Ldp(arm64.X29, arm64.X30, arm64.SP, 64)
+					e.AddImm(arm64.SP, arm64.SP, 80)
+					e.Ret()
+
+				case *ir.BranchTerm:
+					var condReg arm64.Register = arm64.X8
+					if vCond, ok := term.Cond.(*ir.Value); ok {
+						loc := locs[vCond.ID]
+						if loc.IsReg {
+							condReg = arm64ScratchRegs[loc.Reg]
+						}
+					}
+					// If cond != 0, branch to Then
+					branchOffset := len(e.Code)
+					e.Cbnz(condReg, 0)
+					branchFixups = append(branchFixups, branchFixupARM64{
+						offset:   branchOffset,
+						targetBB: term.Then,
+						isCond:   true,
+						condReg:  condReg,
+					})
+
+					// Else branch
+					jumpOffset := len(e.Code)
+					e.B(0)
+					branchFixups = append(branchFixups, branchFixupARM64{
+						offset:   jumpOffset,
+						targetBB: term.Else,
+						isCond:   false,
+					})
+
+				case *ir.JumpTerm:
+					// Emit phi incoming assignments for target basic block
+					for _, phi := range term.Target.Phis {
+						for _, inc := range phi.Incoming {
+							if inc.Block == bb {
+								dstLoc := locs[phi.Res.ID]
+								if dstLoc.IsReg {
+									dstReg := arm64ScratchRegs[dstLoc.Reg]
+									if v, ok := inc.Value.(*ir.Value); ok {
+										srcLoc := locs[v.ID]
+										if srcLoc.IsReg {
+											srcReg := arm64ScratchRegs[srcLoc.Reg]
+											if dstReg != srcReg {
+												e.MovReg(dstReg, srcReg)
+											}
+										}
+									} else if c, ok := inc.Value.(ir.ConstNumber); ok {
+										if c.Value < 0 {
+											e.Movz(dstReg, uint16(-c.Value))
+											e.Sub(dstReg, arm64.XZR, dstReg)
+										} else {
+											e.Movz(dstReg, uint16(c.Value))
+										}
+									} else if s, ok := inc.Value.(ir.ConstString); ok {
+										strOffset := len(e.Code)
+										e.Adr(dstReg, 0)
+										strFixups = append(strFixups, stringFixupARM64{
+											offset:    strOffset,
+											targetReg: dstReg,
+											str:       s.Value,
+										})
+									}
+								}
+							}
+						}
+					}
+
+					jumpOffset := len(e.Code)
+					e.B(0)
+					branchFixups = append(branchFixups, branchFixupARM64{
+						offset:   jumpOffset,
+						targetBB: term.Target,
+						isCond:   false,
+					})
+				}
+			}
+		}
+	}
+
+	// Emit ts_print_val (decimal printer)
+	fnOffsets["ts_print_val"] = len(e.Code)
+	emitARM64PrintVal(e)
+	fnOffsets["ts_print_bool"] = fnOffsets["ts_print_val"]
+
+	// Emit ts_print_str (string printer)
+	fnOffsets["ts_print_str"] = len(e.Code)
+	emitARM64PrintStr(e)
+
+	// Emit ts_alloc (heap bump allocator)
+	fnOffsets["ts_alloc"] = len(e.Code)
+	emitARM64Alloc(e)
+
+	// Emit ts_string_concat (string concatenator). ARM64 keeps the owned-loop
+	// builder helpers semantically correct but currently falls back to immutable
+	// concat; the mutable-capacity fast path is Linux AMD64 specific.
+	fnOffsets["ts_string_concat"] = len(e.Code)
+	emitARM64StringConcat(e, fnOffsets["ts_alloc"])
+	fnOffsets["ts_string_append_owned"] = fnOffsets["ts_string_concat"]
+	fnOffsets["ts_string_builder_seed"] = len(e.Code)
+	e.Ret()
+
+	// Emit ts_sys_exit
+	fnOffsets["ts_sys_exit"] = len(e.Code)
+	emitARM64SysExit(e)
+
+	// Emit String Constants Table
+	strOffsets := make(map[string]int)
+	for _, sf := range strFixups {
+		if _, exists := strOffsets[sf.str]; !exists {
+			for len(e.Code)%8 != 0 {
+				e.Code = append(e.Code, 0)
+			}
+			strOffsets[sf.str] = len(e.Code)
+
+			var lenBuf [8]byte
+			binary.LittleEndian.PutUint64(lenBuf[:], uint64(len(sf.str)))
+			e.Code = append(e.Code, lenBuf[:]...)
+			e.Code = append(e.Code, []byte(sf.str)...)
+			e.Code = append(e.Code, 0)
+		}
+	}
+
+	// Fix up string ADR instructions
+	for _, sf := range strFixups {
+		targetAddr := strOffsets[sf.str]
+		disp := int32(targetAddr - sf.offset)
+		imm21 := uint32(disp) & 0x1FFFFF
+		immlo := imm21 & 0x3
+		immhi := (imm21 >> 2) & 0x7FFFF
+		inst := 0x10000000 | (immlo << 29) | (immhi << 5) | uint32(sf.targetReg)
+		binary.LittleEndian.PutUint32(e.Code[sf.offset:], inst)
+	}
+
+	// Fix up function calls
+	for _, cf := range callFixups {
+		targetAddr, exists := fnOffsets[cf.callee]
+		if !exists {
+			return nil, fmt.Errorf("unresolved call target %q", cf.callee)
+		}
+		wordOffset := int32((targetAddr - cf.offset) / 4)
+		binary.LittleEndian.PutUint32(e.Code[cf.offset:], 0x94000000|(uint32(wordOffset)&0x03FFFFFF))
+	}
+
+	// Fix up local branches
+	for _, bf := range branchFixups {
+		targetAddr, exists := bbOffsets[bf.targetBB]
+		if !exists {
+			return nil, fmt.Errorf("unresolved basic block target %p", bf.targetBB)
+		}
+		wordOffset := int32((targetAddr - bf.offset) / 4)
+		if bf.isCond {
+			// CBNZ condReg, wordOffset
+			binary.LittleEndian.PutUint32(e.Code[bf.offset:], 0xB5000000|((uint32(wordOffset)&0x7FFFF)<<5)|uint32(bf.condReg))
+		} else {
+			// B wordOffset
+			binary.LittleEndian.PutUint32(e.Code[bf.offset:], 0x14000000|(uint32(wordOffset)&0x03FFFFFF))
+		}
+	}
+
+	return e.Code, nil
+}
+
+func emitARM64PrintVal(e *arm64.Emitter) {
+	// Frame setup:
+	// SUB SP, SP, #48
+	// STP X29, X30, [SP, #32]
+	// ADD X29, SP, #32
+	e.SubImm(arm64.SP, arm64.SP, 48)
+	e.Stp(arm64.X29, arm64.X30, arm64.SP, 32)
+	e.AddImm(arm64.X29, arm64.SP, 32)
+
+	// Buffer ends at SP+31. Put newline '\n' at SP+30
+	e.AddImm(arm64.X1, arm64.SP, 30)
+	e.Movz(arm64.X2, 10) // '\n'
+	e.Strb(arm64.X2, arm64.X1, 0)
+
+	// Sign handling: X5 = 1 if negative, 0 otherwise
+	e.Movz(arm64.X5, 0)
+	e.Cmp(arm64.X0, arm64.XZR)
+	geOffset := len(e.Code)
+	e.BCond(arm64.CondGE, 0)
+	e.Movz(arm64.X5, 1)
+	e.Sub(arm64.X0, arm64.XZR, arm64.X0) // X0 = -X0
+	notNegOffset := len(e.Code)
+	binary.LittleEndian.PutUint32(e.Code[geOffset:], 0x54000000|((uint32(int32((notNegOffset-geOffset)/4)&0x7FFFF)<<5)|uint32(arm64.CondGE)))
+
+	// Check if X0 == 0
+	cbnzOffset := len(e.Code)
+	e.Cbnz(arm64.X0, 0) // placeholder fixup to loop
+
+	// if X0 == 0: write '0'
+	e.SubImm(arm64.X1, arm64.X1, 1)
+	e.Movz(arm64.X2, 48) // '0'
+	e.Strb(arm64.X2, arm64.X1, 0)
+
+	jumpToPrintOffset := len(e.Code)
+	e.B(0) // placeholder fixup to print
+
+	// Loop for non-zero digits:
+	loopOffset := len(e.Code)
+	// Patch cbnzOffset to jump here:
+	binary.LittleEndian.PutUint32(e.Code[cbnzOffset:], 0xB5000000|(uint32(int32((loopOffset-cbnzOffset)/4)&0x7FFFF)<<5)|uint32(arm64.X0))
+
+	cbzOffset := len(e.Code)
+	e.Cbz(arm64.X0, 0) // placeholder fixup to print when X0 == 0
+
+	e.Movz(arm64.X2, 10)
+	e.Sdiv(arm64.X3, arm64.X0, arm64.X2) // X3 = X0 / 10
+	e.Mul(arm64.X4, arm64.X3, arm64.X2)  // X4 = X3 * 10
+	e.Sub(arm64.X4, arm64.X0, arm64.X4)  // X4 = remainder
+	e.AddImm(arm64.X4, arm64.X4, 48)     // digit char
+	e.SubImm(arm64.X1, arm64.X1, 1)
+	e.Strb(arm64.X4, arm64.X1, 0)
+	e.MovReg(arm64.X0, arm64.X3)
+
+	loopBackOffset := len(e.Code)
+	e.B(int32((loopOffset - loopBackOffset) / 4))
+
+	printOffset := len(e.Code)
+	// Patch jumpToPrintOffset:
+	binary.LittleEndian.PutUint32(e.Code[jumpToPrintOffset:], 0x14000000|(uint32(int32((printOffset-jumpToPrintOffset)/4))&0x03FFFFFF))
+	// Patch cbzOffset:
+	binary.LittleEndian.PutUint32(e.Code[cbzOffset:], 0xB4000000|(uint32(int32((printOffset-cbzOffset)/4)&0x7FFFF)<<5)|uint32(arm64.X0))
+
+	// Prepend '-' if X5 != 0
+	cbzSignOffset := len(e.Code)
+	e.Cbz(arm64.X5, 0)
+	e.SubImm(arm64.X1, arm64.X1, 1)
+	e.Movz(arm64.X2, 45) // '-'
+	e.Strb(arm64.X2, arm64.X1, 0)
+	afterSignOffset := len(e.Code)
+	binary.LittleEndian.PutUint32(e.Code[cbzSignOffset:], 0xB4000000|(uint32(int32((afterSignOffset-cbzSignOffset)/4)&0x7FFFF)<<5)|uint32(arm64.X5))
+
+	// Print syscall:
+	// Length = (SP + 31) - X1
+	e.AddImm(arm64.X2, arm64.SP, 31)
+	e.Sub(arm64.X2, arm64.X2, arm64.X1) // X2 = len
+	e.Movz(arm64.X0, 1)                 // X0 = stdout
+	// X1 is already buffer pointer
+	e.Movz(arm64.X16, 4) // Darwin sys_write (4)
+	e.Svc(0x80)
+
+	// Epilogue:
+	e.Ldp(arm64.X29, arm64.X30, arm64.SP, 32)
+	e.AddImm(arm64.SP, arm64.SP, 48)
+	e.Ret()
+}
+
+func emitARM64SysExit(e *arm64.Emitter) {
+	e.Movz(arm64.X0, 0)  // exit code 0
+	e.Movz(arm64.X16, 1) // Darwin sys_exit (1)
+	e.Svc(0x80)
+	e.Ret()
+}
+
+func emitARM64PrintStr(e *arm64.Emitter) {
+	// Frame: 48 bytes (16-byte aligned)
+	e.SubImm(arm64.SP, arm64.SP, 48)
+	e.Stp(arm64.X29, arm64.X30, arm64.SP, 32)
+	e.AddImm(arm64.X29, arm64.SP, 32)
+
+	e.MovReg(arm64.X9, arm64.X0)
+	e.Ldr(arm64.X2, arm64.X9, 0)    // len
+	e.AddImm(arm64.X1, arm64.X9, 8) // data ptr
+	e.Movz(arm64.X0, 1)             // stdout
+	e.Movz(arm64.X16, 4)            // Darwin sys_write
+	e.Svc(0x80)
+
+	// Write newline
+	e.AddImm(arm64.X1, arm64.SP, 16)
+	e.Movz(arm64.X2, 10)
+	e.Strb(arm64.X2, arm64.X1, 0)
+	e.Movz(arm64.X2, 1)
+	e.Movz(arm64.X0, 1)
+	e.Movz(arm64.X16, 4)
+	e.Svc(0x80)
+
+	e.Ldp(arm64.X29, arm64.X30, arm64.SP, 32)
+	e.AddImm(arm64.SP, arm64.SP, 48)
+	e.Ret()
+}
+
+func emitARM64Alloc(e *arm64.Emitter) {
+	pcInText := 1024 + len(e.Code)
+	adrOffset := int32(0x4000 - pcInText)
+	e.Adr(arm64.X1, adrOffset) // X1 points to 0x100004000 (__DATA)
+
+	e.Ldr(arm64.X2, arm64.X1, 0) // current offset
+	cbnzOffset := len(e.Code)
+	e.Cbnz(arm64.X2, 0)
+	e.Movz(arm64.X2, 16)
+
+	hasOffset := len(e.Code)
+	binary.LittleEndian.PutUint32(e.Code[cbnzOffset:], 0xB5000000|(uint32(int32((hasOffset-cbnzOffset)/4)&0x7FFFF)<<5)|uint32(arm64.X2))
+
+	e.Add(arm64.X3, arm64.X1, arm64.X2) // return ptr
+	e.Add(arm64.X2, arm64.X2, arm64.X0) // new offset
+	e.AddImm(arm64.X2, arm64.X2, 15)
+	e.Movz(arm64.X4, 15)
+	e.Bic(arm64.X2, arm64.X2, arm64.X4)
+	e.Str(arm64.X2, arm64.X1, 0)
+	e.MovReg(arm64.X0, arm64.X3)
+	e.Ret()
+}
+
+func emitARM64StringConcat(e *arm64.Emitter, allocOffset int) {
+	e.SubImm(arm64.SP, arm64.SP, 80)
+	e.Stp(arm64.X29, arm64.X30, arm64.SP, 64)
+	e.AddImm(arm64.X29, arm64.SP, 64)
+	e.Stp(arm64.X19, arm64.X20, arm64.SP, 48)
+	e.Stp(arm64.X21, arm64.X22, arm64.SP, 32)
+	e.Stp(arm64.X23, arm64.X24, arm64.SP, 16)
+
+	e.MovReg(arm64.X19, arm64.X0) // a
+	e.MovReg(arm64.X20, arm64.X1) // b
+
+	e.Ldr(arm64.X21, arm64.X19, 0) // len_a
+	e.Ldr(arm64.X22, arm64.X20, 0) // len_b
+
+	e.Add(arm64.X23, arm64.X21, arm64.X22) // total_len
+
+	e.AddImm(arm64.X0, arm64.X23, 16) // alloc size
+	callAllocOffset := len(e.Code)
+	e.Bl(int32((allocOffset - callAllocOffset) / 4))
+
+	e.MovReg(arm64.X24, arm64.X0) // new_str
+
+	e.Str(arm64.X23, arm64.X24, 0) // store total_len
+
+	// Copy a
+	e.AddImm(arm64.X9, arm64.X19, 8)  // src_a
+	e.AddImm(arm64.X10, arm64.X24, 8) // dst
+	e.Movz(arm64.X11, 0)
+
+	cbzCopyA := len(e.Code)
+	e.Cbz(arm64.X21, 0)
+
+	loopCopyA := len(e.Code)
+	e.Ldrb(arm64.X12, arm64.X9, 0)
+	e.Strb(arm64.X12, arm64.X10, 0)
+	e.AddImm(arm64.X9, arm64.X9, 1)
+	e.AddImm(arm64.X10, arm64.X10, 1)
+	e.AddImm(arm64.X11, arm64.X11, 1)
+	e.Cmp(arm64.X11, arm64.X21)
+	loopBackA := len(e.Code)
+	e.BCond(arm64.CondLT, int32((loopCopyA-loopBackA)/4))
+
+	afterCopyA := len(e.Code)
+	binary.LittleEndian.PutUint32(e.Code[cbzCopyA:], 0xB4000000|(uint32(int32((afterCopyA-cbzCopyA)/4)&0x7FFFF)<<5)|uint32(arm64.X21))
+
+	// Copy b
+	e.AddImm(arm64.X9, arm64.X20, 8) // src_b
+	e.Movz(arm64.X11, 0)
+
+	cbzCopyB := len(e.Code)
+	e.Cbz(arm64.X22, 0)
+
+	loopCopyB := len(e.Code)
+	e.Ldrb(arm64.X12, arm64.X9, 0)
+	e.Strb(arm64.X12, arm64.X10, 0)
+	e.AddImm(arm64.X9, arm64.X9, 1)
+	e.AddImm(arm64.X10, arm64.X10, 1)
+	e.AddImm(arm64.X11, arm64.X11, 1)
+	e.Cmp(arm64.X11, arm64.X22)
+	loopBackB := len(e.Code)
+	e.BCond(arm64.CondLT, int32((loopCopyB-loopBackB)/4))
+
+	afterCopyB := len(e.Code)
+	binary.LittleEndian.PutUint32(e.Code[cbzCopyB:], 0xB4000000|(uint32(int32((afterCopyB-cbzCopyB)/4)&0x7FFFF)<<5)|uint32(arm64.X22))
+
+	// Null-terminate
+	e.Movz(arm64.X12, 0)
+	e.Strb(arm64.X12, arm64.X10, 0)
+
+	e.MovReg(arm64.X0, arm64.X24)
+
+	e.Ldp(arm64.X23, arm64.X24, arm64.SP, 16)
+	e.Ldp(arm64.X21, arm64.X22, arm64.SP, 32)
+	e.Ldp(arm64.X19, arm64.X20, arm64.SP, 48)
+	e.Ldp(arm64.X29, arm64.X30, arm64.SP, 64)
+	e.AddImm(arm64.SP, arm64.SP, 80)
+	e.Ret()
+}
