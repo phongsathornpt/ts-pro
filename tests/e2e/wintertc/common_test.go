ae01e865d20460019b7a6d5fe6cb17c45fdfcba6
thorn <37927965+phongsathornpt@users.noreply.github.com>
2026-09-13 12:51:12 +0700

    test(wintertc): parallelize only isolated deterministic cases

--- tests/e2e/wintertc/common_test.go
+++ tests/e2e/wintertc/common_test.go
@@ -1,6 +1,7 @@
 package wintertc_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/phongsathornpt/ts-pro/tests/e2e/harness"
@@ -14,12 +15,41 @@ type linuxAMD64Case struct {
 
 func runLinuxAMD64(t *testing.T, tc linuxAMD64Case) {
 	t.Helper()
-	t.Parallel()
+	if winterTCParallelSafe(t.Name()) {
+		t.Parallel()
+	}
 	harness.RunLinuxAMD64(t, harness.LinuxAMD64Case{
 		Name:     tc.name,
 		Source:   tc.source,
 		Expected: tc.expected,
 	})
+}
+
+func winterTCParallelSafe(testName string) bool {
+	if strings.HasPrefix(testName, "TestLinuxAMD64WinterTCURL") ||
+		strings.HasPrefix(testName, "TestLinuxAMD64WinterTCBlob") {
+		return true
+	}
+
+	switch testName {
+	case "TestCryptoGetRandomValues",
+		"TestLinuxAMD64WinterTCTypedArrays",
+		"TestLinuxAMD64WinterTCTextEncoding",
+		"TestLinuxAMD64WinterTCBodyJSONScalars",
+		"TestLinuxAMD64RuntimeJSONStructuredConformance",
+		"TestLinuxAMD64WinterTCBodyJSONStructured",
+		"TestLinuxAMD64WinterTCQueuingStrategies",
+		"TestLinuxAMD64WinterTCReadableStreamCore",
+		"TestLinuxAMD64WinterTCWritableStreamCore",
+		"TestLinuxAMD64WinterTCTransformStreamPipe",
+		"TestLinuxAMD64WinterTCStreamsConformance",
+		"TestLinuxAMD64WinterTCBodyStreamBacking",
+		"TestLinuxAMD64WinterTCBodyStreamDisturbance",
+		"TestLinuxAMD64WinterTCBodyLockedIsUnusable":
+		return true
+	default:
+		return false
+	}
 }
 
 func mustReadExample(t *testing.T, path string) string {
